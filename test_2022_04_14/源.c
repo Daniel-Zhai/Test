c1154bb491f12d32a03b666b2c4a58e5f18a14b1
Daniel <2553182585@qq.com>
2022-04-15 00:30:34 +0800

    haffmantree

--- test_2022_04_14/源.c
+++ test_2022_04_14/源.c
@@ -1,21 +1,25 @@
 #define _CRT_SECURE_NO_WARNINGS
-
+#include<stdlib.h>
 #include<stdio.h>
 typedef struct
 {
 	int weight, lch, rch, parent;
 }HaffmanNode, *HaffmanTree;
 
-int Select(HaffmanTree T, int n, int s1,int s2)
+int Select(HaffmanTree T, int n)
 {
 	int i = 0;
-	for (i = 1; i <= n; i++)
+	int min = 0;
+	for (i = 2; i <= n; i++)
 	{
+		min = 1;
 		if (T[i].parent == 0)
 		{
-
+			if (T[min].weight > T[i].weight)
+				min = i;
 		}
 	}
+	return min;
 }
 void CreateHaffmanTree(HaffmanTree T, int n)
 {
@@ -36,18 +40,30 @@ void CreateHaffmanTree(HaffmanTree T, int n)
 	}
 	for (i = n + 1; i <= m; i++)
 	{
-		Select(T, i - 1, s1, s2);
+		int s1 = Select(T, i - 1);
 		T[s1].parent = i;
-		T[s2].parent = i;
 		T[i].lch = s1;
+		int s2 = Select(T, i - 1);
+		T[s2].parent = i;
 		T[i].rch = s2;
 		T[i].weight = T[s1].weight + T[s2].weight;
+	}
+}
+void print(HaffmanTree T,int n)
+{
+	int i = 0; int a = n;
+	printf("    weight   parent   lch   rch\n");
+	for (i = 1; i <= 2a - 1; i++)
+	{
+		printf("%d   %d  %d   %d   %d\n", i, T[i].weight, T[i].parent, T[i].lch, T[i].rch);
 	}
 }
 int main()
 {
 	int n = 0;
 	scanf("%d", &n);
-	HaffmanTree T;
-	CreateHaffmanTree(T, n)
+	HaffmanNode T[100] = {0};
+	CreateHaffmanTree(T, n);
+	print(T,n);
 }
+
